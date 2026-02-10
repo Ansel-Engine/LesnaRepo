73a9a62b6ed1e81033cfcf8063ffb73a305bb7cb
Ansel-Engine <lesnab@yahoo.co.uk>
2026-02-10 10:32:34 +0000

    Committing 10 items from workspace 8bc6ea08-fd30-4ca7-be7f-dc76aee17419

---
+++ Silver_Warehouse_(ODS).Warehouse/dbo/Tables/spanishspeakingcountries.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[spanishspeakingcountries] (
+
+	[ODS_KEY] varchar(8000) NULL, 
+	[country_name] varchar(8000) NULL, 
+	[official_country_name] varchar(8000) NULL, 
+	[flag_description] varchar(8000) NULL, 
+	[area] float NULL, 
+	[load_date] datetime2(6) NULL
+);
